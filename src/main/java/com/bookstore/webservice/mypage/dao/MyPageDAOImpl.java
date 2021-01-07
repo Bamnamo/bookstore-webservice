e6957093c7c1d939bf4573e4cc6d1dfe48416c21
박성범 <shivash@naver.com>
2021-01-07 14:35:25 +0900

    5.1.3 마이페이지 jsp 설정

--- src/main/java/com/bookstore/webservice/mypage/dao/MyPageDAOImpl.java
+++
@@ -1,4 +0,0 @@
-package com.bookstore.webservice.mypage.dao;
-
-public class MyPageDAOImpl {
-}
--- src/main/java/com/bookstore/webservice/mypage/vo/MyPageVO.java
+++ src/main/java/com/bookstore/webservice/mypage/vo/MyPageVO.java
@@ -1,8 +1,11 @@
 package com.bookstore.webservice.mypage.vo;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class MyPageVO {
     private String member_id;
     private String beginDate;

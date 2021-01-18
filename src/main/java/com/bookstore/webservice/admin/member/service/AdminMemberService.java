28f4fb8cf933076a37d892b974a15a526d7b25d1
박성범 <shivash@naver.com>
2021-01-18 15:31:36 +0900

    6.3.6 회원관리 기능 오류 수정

--- src/main/java/com/bookstore/webservice/admin/member/service/AdminMemberService.java
+++ src/main/java/com/bookstore/webservice/admin/member/service/AdminMemberService.java
@@ -7,4 +7,6 @@ import java.util.HashMap;
 
 public interface AdminMemberService {
     public ArrayList<MemberVO> listMember(HashMap condMap) throws Exception;
+    public MemberVO memberDetail(String member_id) throws Exception;
+    public void  modifyMemberInfo(HashMap memberMap) throws Exception;
 }

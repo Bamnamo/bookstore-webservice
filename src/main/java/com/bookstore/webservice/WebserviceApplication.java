b4a59104d9e53b58d48c9ac203c593e6f72fd0c1
박성범 <shivash@naver.com>
2021-02-08 14:06:05 +0900

    7.2.9 war 패키지 설정

--- src/main/java/com/bookstore/webservice/WebserviceApplication.java
+++ src/main/java/com/bookstore/webservice/WebserviceApplication.java
@@ -2,12 +2,19 @@ package com.bookstore.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebserviceApplication {
+public class WebserviceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebserviceApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebserviceApplication.class);
     }
 
 }

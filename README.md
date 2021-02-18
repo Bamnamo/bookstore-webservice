65ac8c5d73980b1450ddf238acde3104a11408f1
박성범 <shivash@naver.com>
2021-02-18 13:17:31 +0900

    7.4.2 Read.me 문서 작성 및 프로젝트 간단한설명

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-#BookStore 
+BOOKTOPIA(2020)
+==============
+
+Link: http://13.209.33.41/main/main.do
+
+#설명
+SpringBoot Framework를 이용한 온라인 서점 프로젝트
+
+#사용 기술
++ JAVA
++ SpringBoot
++ MyBatis
++ Oracle
++ JSP
++ AWS EC2
++ AWS RDS
++ Travis-CI
+
+
+
+
 
 
--- src/main/java/com/bookstore/webservice/goods/dao/GoodsDAO.java
+++ src/main/java/com/bookstore/webservice/goods/dao/GoodsDAO.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface GoodsDAO {
     public List<GoodsVO> selectGoodsList(String goodsStatus ) throws DataAccessException;
     public List<String> selectKeywordSearch(String keyword) throws DataAccessException;
-    public GoodsVO selectGoodsDetail(String goods_id) throws DataAccessException;
-    public List<ImageFileVO> selectGoodsDetailImage(String goods_id) throws DataAccessException;
+    public GoodsVO selectGoodsDetail(String goodsId) throws DataAccessException;
+    public List<ImageFileVO> selectGoodsDetailImage(String goodsId) throws DataAccessException;
     public List<GoodsVO> selectGoodsBySearchWord(String searchWord) throws DataAccessException;
 }
